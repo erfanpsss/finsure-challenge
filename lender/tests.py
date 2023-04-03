a4a73ad7aa5a35a087fc399f0015ed9fa66d0216
erfanpsss <erfanpsss@hotmail.com>
2023-04-03 20:51:09 +1000

    added a simple api test

--- lender/tests.py
+++ lender/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestLenderViewSet(APITestCase):
+
+    def test_create(self):
+        url = reverse("lenders")
+        data = {
+            "name": "Commonwealth Bank",
+            "code": "CBA",
+            "upfront_commission_rate": 0,
+            "trial_commission_rate": 1,
+            "active": True
+        }
+        res = self.client.post(url, data=data)
+        self.assertEqual(res.status_code, 201)
+

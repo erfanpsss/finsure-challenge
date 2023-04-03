0ea49cec6961a9c6cc182a26267f9e12350b41ad
erfanpsss <erfanpsss@hotmail.com>
2023-04-03 20:20:26 +1000

    Enhanced LenderBulkSerializer

--- lender/serializers.py
+++ lender/serializers.py
@@ -18,11 +18,22 @@ class LenderBulkSerializer(serializers.Serializer):
         return csv_data
 
     def create(self, validated_data):
+        report = {
+            "created_count": 0,
+            "failed_count": 0,
+            "failed_items": []
+        }
         for lender in validated_data.get("file"):
-            Lender.objects.create(
-                name=lender.get("name"),
-                code=lender.get("code"),
-                upfront_commission_rate=lender.get("upfront_commission_rate"),
-                trial_commission_rate=lender.get("trial_commission_rate"),
-                active=True if lender.get("active", "").lower() == "true" else False
-            )
+            try:
+                Lender.objects.create(
+                    name=lender.get("name"),
+                    code=lender.get("code"),
+                    upfront_commission_rate=lender.get("upfront_commission_rate"),
+                    trial_commission_rate=lender.get("trial_commission_rate"),
+                    active=True if lender.get("active", "").lower() == "true" else False
+                )
+                report["created_count"] += 1
+            except Exception as error:
+                report["failed_count"] += 1
+                report["failed_items"].append(f'{", ".join(lender)}, error: {str(error)}')
+        return report
--- lender/views.py
+++ lender/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
 from rest_framework import viewsets, parsers
 from .models import Lender
 from .serializers import LenderSerializer, LenderBulkSerializer
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import RetrieveModelMixin
 from django.shortcuts import HttpResponse
 from rest_framework.pagination import PageNumberPagination
-import json
 import csv
 
 class LenderPagination(PageNumberPagination):
@@ -34,20 +30,6 @@ class LenderViewSet(viewsets.ModelViewSet):
             return queryset.filter(active=True)
         return queryset
 
-    def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
-    
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-    
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-    
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-    
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
     
 
 class LenderBulkImportViewSet(viewsets.ViewSet):
@@ -69,8 +51,12 @@ class LenderBulkImportViewSet(viewsets.ViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.create(serializer.validated_data)
-        return Response("Lenders data have been imported")
+        report = serializer.create(serializer.validated_data)
+        return Response(
+            f"{report.get('created_count')} created, "
+            f"{report.get('failed_count')} failed, "
+            f"failed items: {', '.join(report.get('failed_items'))}"
+        )
     
 
 class LenderBulkExportViewSet(viewsets.ViewSet):
